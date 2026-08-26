5cd9836edc26266f6bdf57ff8ef18a2e7aee7aa9
Hilal Kaya <kayahll2428@gmail.com>
2026-08-26 10:55:30 +0300

    Initialize Spring Boot application

---
+++ src/main/java/com/nexamarket/nexamarket/NexamarketApplication.java
@@ -0,0 +1,13 @@
+package com.nexamarket.nexamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexamarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexamarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexamarket/nexamarket/NexamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexamarket.nexamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
