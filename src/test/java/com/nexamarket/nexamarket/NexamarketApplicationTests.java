b4b5454668268b92581b6922cdd57db3d0feda1d
Hilal Kaya <kayahll2428@gmail.com>
2026-08-26 10:58:03 +0300

    Configure PostgreSQL development environment

--- src/test/java/com/nexamarket/nexamarket/NexamarketApplicationTests.java
+++ src/test/java/com/nexamarket/nexamarket/NexamarketApplicationTests.java
@@ -2,8 +2,10 @@ package com.nexamarket.nexamarket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NexamarketApplicationTests {
 
 	@Test
