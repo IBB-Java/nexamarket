270b9e3ba1fb16bb1b215f29ae8d0e38a6b856df
Eyüp <eyup.yavuz6534@gmail.com>
2026-08-26 11:50:48 +0300

    İlk commit: Proje altyapısı ve Entity sınıfları eklendi

---
+++ src/test/java/com/nexamarket/NexaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
